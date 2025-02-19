a7df7af82172bf9fd03133cf822c438a79cc9061
GUPTA-AMBAR <ambargupta1290@gamil.com>
2025-02-19 10:46:59 +0530

    change

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "../../components/shared/form/Form";
 import { useSelector } from 'react-redux';
 import Spinner from "../../components/shared/Spinner";
